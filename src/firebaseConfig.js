ba62cf631eeacff85de832011593ba245afd02fc
TanvirRahmanTaiyeb <140767834+TanvirRahmanTaiyeb@users.noreply.github.com>
2025-07-28 18:47:29 +1000

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,16 +1,16 @@
-// Importing the functions need from the SDKs
+// Importing the functions needed from the SDKs
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// My web app's Firebase configuration
+// Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAEXi5F33-42nqzoGaGSA4x-bK-xZWGvPg",
-  authDomain: "REMOVED_PROJECT_ID.firebaseapp.com",
-  projectId: "REMOVED_PROJECT_ID",
-  storageBucket: "gs://REMOVED_PROJECT_ID.appspot.com", // Updated storage bucket
-  messagingSenderId: "776304936141",
-  appId: "1:776304936141:web:e07776f6fac3d936735584",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: `gs://${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
